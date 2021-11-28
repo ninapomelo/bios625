3790ce4e2a23ac2e20c55447f51f5fc3b69b9779
ninapomelo <panyue@kean.edu>
2021-11-28 12:26:13 -0500

    change

---
+++ R/square.R
@@ -0,0 +1,14 @@
+#'Square
+#'
+#'Gets the square of a number #'
+#'@param x input value
+#'
+#'@return the square of x
+#'
+#'@examples
+#'square(3)
+#'
+#'@export
+#'
+square = function(x) {
+  return (x^2) }
